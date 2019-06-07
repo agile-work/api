706900301341ed14eab8d637f056f283751c948c
Bruno Piaui <brunopiaui@gmail.com>
2019-06-07 11:19:04 -0300

    Changing authorization to security middleware

--- middlewares/security.go
+++ middlewares/security.go
@@ -7,11 +7,40 @@ import (
 	"strings"
 
 	"github.com/agile-work/srv-shared/sql-builder/builder"
+	"github.com/dgrijalva/jwt-go"
 
 	"github.com/agile-work/srv-mdl-shared/models"
 	shared "github.com/agile-work/srv-shared"
 	"github.com/agile-work/srv-shared/sql-builder/db"
 )
+
+// Authorization validates the token and insert user data in the request
+func Authorization(next http.Handler) http.Handler {
+	// TODO: Pensar em um jeito talvez de desconsiderar o token e utilizar o certificado p/ garantir a segurança na comunicação dos serviços com a api
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.RequestURI, "/auth/login") {
+			token, err := jwt.ParseWithClaims(r.Header.Get("Authorization"), &models.UserCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+				return []byte("AllYourBase"), nil // TODO: Check the best place for this key, probably the config.toml
+			})
+			if err != nil {
+				http.Error(w, http.StatusText(401), http.StatusUnauthorized)
+				return
+			}
+			if token != nil && token.Valid {
+				claims := token.Claims.(*models.UserCustomClaims)
+				r.Header.Add("userID", claims.User.ID)
+				if r.Header.Get("Content-Language") == "" {
+					r.Header.Add("Content-Language", claims.User.LanguageCode)
+				}
+			} else {
+				http.Error(w, http.StatusText(401), http.StatusUnauthorized)
+				return
+			}
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 // GetUser set user information to security on request
 func GetUser(next http.Handler) http.Handler {
