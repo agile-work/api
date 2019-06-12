550df0c9129ecd5b187e12c893c6a5f1b7e6d07e
Bruno Piaui <brunopiaui@gmail.com>
2019-06-12 11:01:57 -0300

    Removing connection to database Removing middleware to getuser

--- middlewares/security.go
+++ middlewares/security.go
@@ -1,17 +1,12 @@
 package middlewares
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 	"strings"
 
-	"github.com/agile-work/srv-shared/sql-builder/builder"
 	"github.com/dgrijalva/jwt-go"
 
 	"github.com/agile-work/srv-mdl-shared/models"
-	shared "github.com/agile-work/srv-shared"
-	"github.com/agile-work/srv-shared/sql-builder/db"
 )
 
 // Authorization validates the token and insert user data in the request
@@ -35,35 +30,6 @@ func Authorization(next http.Handler) http.Handler {
 			} else {
 				http.Error(w, http.StatusText(401), http.StatusUnauthorized)
 				return
-			}
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-// GetUser set user information to security on request
-func GetUser(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.Contains(r.RequestURI, "/core/instances") {
-			userID := r.Header.Get("userID")
-			if userID != "" {
-				user := models.User{}
-				userIDColumn := fmt.Sprintf("%s.id", shared.TableCoreUsers)
-				condition := builder.Equal(userIDColumn, userID)
-				err := db.LoadStruct(shared.TableCoreUsers, &user, condition)
-				if err != nil {
-					http.Error(w, http.StatusText(401), http.StatusUnauthorized)
-					return
-				}
-				user.Password = ""
-
-				userByte, err := json.Marshal(user)
-				if err != nil {
-					http.Error(w, http.StatusText(401), http.StatusUnauthorized)
-					return
-				}
-				r.Header.Add("User", string(userByte))
 			}
 		}
 
